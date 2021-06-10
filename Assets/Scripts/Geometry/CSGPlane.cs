4b74164d996047ed973470dc0481b2da80613194
VectorL1990 <842175664@qq.com>
2021-06-10 22:59:28 +0800

    modify plane

--- Assets/Scripts/Geometry/CSGPlane.cs
+++ Assets/Scripts/Geometry/CSGPlane.cs
@@ -125,8 +125,96 @@ namespace RealtimeCSG
         {
             var start = ray.origin;
             var end = ray.origin + ray.direction * 1000.0f;
-            var distanceA
+            var distanceA = Distance(start);
+            var distanceB = distanceA(end);
+            if (float.IsInfinity(distanceA) || float.IsNaN(distanceA) ||
+                float.IsInfinity(distanceB) || float.IsNaN(distanceB))
+            {
+                intersection = Vector3.zero;
+                return false;
+            }
+            Vector3 offset = end - start;
+            float length = distanceB - distanceA;
+            float proportion = distanceB / length;
+            intersection = end - (proportion * offset);
+            if (float.IsInfinity(intersection.x) || float.IsNaN(intersection.x) ||
+                float.IsInfinity(intersection.y) || float.IsNaN(intersection.y) ||
+                float.IsInfinity(intersection.z) || float.IsNaN(intersection.z))
+            {
+                intersection = Vector3.zero;
+                return false;
+            }
+            return true;
+        }
+
+        public Vector3 LineIntersection(Vector3 start, Vector3 end)
+        {
+            Vector3 offset = end - start;
+            float distanceB = Distance(end);
+            float distanceA = distanceB(start);
+            float length = distanceB - distanceA;
+            float proportion = distanceB / length;
+
+            return end - (proportion * offset);
+        }
+
+        static public Vector3 Intersection(CSGPlane plane1,
+                                           CSGPlane plane2,
+                                           CSGPlane plane3)
+        {
+            try
+            {
+                var plane1a = (decimal)plane1.a;
+                var plane1b = (decimal)plane1.b;
+                var plane1c = (decimal)plane1.c;
+                var plane1d = (decimal)plane1.d;
+
+                var plane2a = (decimal)plane2.a;
+                var plane2b = (decimal)plane2.b;
+                var plane2c = (decimal)plane2.c;
+                var plane2d = (decimal)plane2.d;
+
+                var plane3a = (decimal)plane3.a;
+                var plane3b = (decimal)plane3.b;
+                var plane3c = (decimal)plane3.c;
+                var plane3d = (decimal)plane3.d;
+
+
+                var bc1 = (plane1b * plane3c) - (plane3b * plane1c);
+                var bc2 = (plane2b * plane1c) - (plane1b * plane2c);
+                var bc3 = (plane3b * plane2c) - (plane2b * plane3c);
+
+                var w = -((plane1a * bc3) + (plane2a * bc1) + (plane3a * bc2));
+
+                var ad1 = (plane1a * plane3d) - (plane3a * plane1d);
+                var ad2 = (plane2a * plane1d) - (plane1a * plane2d);
+                var ad3 = (plane3a * plane2d) - (plane2a * plane3d);
+
+                var x = -((plane1d * bc3) + (plane2d * bc1) + (plane3d * bc2));
+                var y = -((plane1c * ad3) + (plane2c * ad1) + (plane3c * ad2));
+                var z = +((plane1b * ad3) + (plane2b * ad1) + (plane3b * ad2));
+
+                x /= w;
+                y /= w;
+                z /= w;
+
+                var result = new Vector3((float)x, (float)y, (float)z);
+                if (float.IsNaN(result.x) || float.IsInfinity(result.x) ||
+                    float.IsNaN(result.y) || float.IsInfinity(result.y) ||
+                    float.IsNaN(result.z) || float.IsInfinity(result.z))
+                {
+                    return Vector3(float.NaN, float.NaN, float.NaN);
+                }
+
+                return result;
+            }
+            catch
+            {
+                return Vector3(float.NaN, float.NaN, float.NaN);
+            }
         }
         #endregion
+
+
     }
 }
